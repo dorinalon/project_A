2b4932dd884893653e6af88314d3b4f85d86835f
shirili <shirili.s@gmail.com>
2019-09-25 16:16:27 +0300

    fixed only_ppg_GPU

--- one_patient_ppg_only_GPU.py
+++ one_patient_ppg_only_GPU.py
@@ -4,20 +4,23 @@ import math, random
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+import statistics as stat
 from torch.autograd import Variable
 
 
 device = torch.device("cuda")
 
 n_samples = 2
-sample_size = 145000
+sample_size = 298000
+test_len = 5000
+train_len = 10000
 data_ppg = np.zeros((sample_size, n_samples))
 data_bp = np.zeros((sample_size, n_samples))
 
 data_ppg_test_1 = np.zeros((sample_size, n_samples))
 data_bp_test_1 = np.zeros((sample_size, n_samples))
 
-path = './268269-2325-MDC_PRESS_BLD_ART_ABP-125.csv'
+path = '/home/shirili/Downloads/ShirirliDorin/project_A/data/268269-2325-MDC_PRESS_BLD_ART_ABP-125.csv'
 
 i=0
 with open(path) as csv_file:
@@ -27,10 +30,10 @@ with open(path) as csv_file:
         data_bp[i,1] = (float(row[1]))
         data_bp[i,0] = (float(row[1]))
         i=i+1
-        if(i==145000):
+        if(i==298000):
             break
 
-path = './268269-2325-MDC_PULS_OXIM_PLETH-125.csv'
+path = '/home/shirili/Downloads/ShirirliDorin/project_A/data/268269-2325-MDC_PULS_OXIM_PLETH-125.csv'
 
 i=0
 with open(path) as csv_file:
@@ -40,24 +43,30 @@ with open(path) as csv_file:
         data_ppg[i,1] = (float(row[1]))
         data_ppg[i,0] = (float(row[1]))
         i = i + 1
-        if (i == 145000):
+        if (i == 298000):
             break
 #####
 
+max_bp = np.amax(data_bp)
+max_ppg = np.amax(data_ppg)
 
-data_bp = (data_bp/1500) - 0.9
-data_ppg = (data_ppg/2500) -0.8
+data_bp = data_bp/max_bp
+data_ppg = data_ppg/max_ppg
+
+median_bp = stat.median(data_bp[1])
+median_ppg = stat.median(data_ppg[1])
+
+data_bp = data_bp - median_bp
+data_ppg = data_ppg - median_ppg
+
+train_bp = data_bp[:train_len,:]
+train_ppg = data_ppg[:train_len,:]
 
 
-
-train_bp = data_bp[:5000,:]
-train_ppg = data_ppg[:5000,:]
-
-
-test_bp = data_bp[5000:6000,:]
+test_bp = data_bp[train_len:train_len+test_len,:]
 #test_ecg = data_ecg[5000:6000,:]
 #test_ri = data_ri[5000:6000,:]
-test_ppg = data_ppg[5000:6000,:]
+test_ppg = data_ppg[train_len:train_len+test_len,:]
 
 output_dim = 1
 
@@ -101,7 +110,7 @@ r= CustomLSTM( hidden_size, input_dim, output_dim).to(device)
 predictions = []
 optimizer = torch.optim.Adam(r.parameters(), lr=1e-3)
 loss_func = nn.MSELoss().to(device)#nn.L1Loss()
-#print("here")
+
 tau = 0 # future estimation time
 loss_vec = []
 running_loss = 0.0
@@ -113,11 +122,8 @@ out = torch.Tensor(train_bp.reshape((train_bp.shape[0], -1, 1))).cuda()
 #inp_ppg = inp_ppg[:1000,0:1,0:1]
 #out = out[:1000,0:1,0:1]
 
-for t in range(400):
+for t in range(500):
     hidden = None
-
-
-
    # x = x[:-tau]
    # out = out[tau:]
 
@@ -130,7 +136,6 @@ for t in range(400):
     optimizer.zero_grad()
     predictions.append(pred.data.cpu().numpy())
     loss = loss_func(pred, out)
-    #print("here")
 
     loss.backward()
     optimizer.step()
@@ -138,28 +143,30 @@ for t in range(400):
     running_loss = 0.0
     running_loss += loss.item()
     loss_vec.append(running_loss)
-    if t%20==0:
-        print(t, running_loss)
-
-        plt.plot(inp_ppg[:1000,0].data.cpu().numpy(),label='train_ppg')
-        plt.plot(pred[:1000,0].data.cpu().numpy(), label='prediction_BP')
-        plt.plot(out[:1000,0].data.cpu().numpy(), label='train_BP')
+    print(t, running_loss)
+    if t%30==0:
+        #print(t, running_loss)
+        plt.clf()
+        plt.plot(inp_ppg[round(train_len/2):(round(train_len/2)+1000),0].data.cpu().numpy(),label='train_ppg')
+        plt.plot(pred[round(train_len/2):(round(train_len/2)+1000),0].data.cpu().numpy(), label='prediction_BP')
+        plt.plot(out[round(train_len/2):(round(train_len/2)+1000),0].data.cpu().numpy(), label='train_BP')
         plt.title('iteration '+str(t))
         plt.legend()
 
-        plt.show()
+        #plt.show()
 
 
-
+plt.clf()
 plt.plot(loss_vec)
 plt.title("Mean loss")
 plt.xlabel('iteration')
 plt.ylabel('loss')
 plt.show()
 
-# TEST
 
-t_inp_ppg = Variable(torch.Tensor(test_ppg.reshape((test_ppg.shape[0], -1, 1))), requires_grad=False)
+
+# TEST
+t_inp_ppg = torch.Tensor(test_ppg.reshape((test_ppg.shape[0], -1, 1))).cuda()
 
 
 #x_t = torch.cat((t_inp_ecg,t_inp_ri,t_inp_ppg), dim=2)
@@ -169,16 +176,16 @@ pred_t = r(t_inp_ppg)
 
 # Test loss
 runningLossTest = 0.0
-lossTest = loss_func(pred_t, Variable(torch.Tensor(test_bp.reshape((test_bp.shape[0], -1, 1)))))
+lossTest = loss_func(pred_t, (torch.Tensor(test_bp.reshape((test_bp.shape[0], -1, 1)))).cuda())
 runningLossTest += lossTest.item()
 
 # plot the mean error for each iteration
-for j in range(1000):
-    a = pred_t[j,1].data.numpy()
-    pred_t[j,1] = (((a[0] + 0.8) * 2800) * 0.0625) - 40
-test_bp[:,1] = ((((test_bp[:,1]) + 0.8 ) * 2800) * 0.0625) - 40
-plt.plot(pred_t[:1000,1].data.numpy(), label ='prediction_BP')
-plt.plot(test_bp[:1000,1], label ='expected_BP')
+for j in range(test_len):
+    a = pred_t[j,1].data.cpu().numpy()
+    pred_t[j,1] = (((a[0] + median_bp) * max_bp) * 0.0625) - 40
+test_bp[:,1] = ((((test_bp[:,1]) + median_bp ) * max_bp) * 0.0625) - 40
+plt.plot(pred_t[round(test_len/2):round(test_len/2)+2000,1].data.cpu().numpy(), label ='prediction_BP')
+plt.plot(test_bp[round(test_len/2):round(test_len/2)+2000,1], label ='expected_BP')
 plt.xlabel('samples')
 plt.ylabel('mmHg')
 plt.title("2728529-6534 - BP Prediction based on PPG")
@@ -186,20 +193,20 @@ plt.legend()
 plt.show()
 
 
-path = '/media/dorin/A4E65F96E65F6796/Project A/one_pers_data/2728529-6534-expec0_pred1.csv'
+path = '/home/shirili/Downloads/ShirirliDorin/project_A/out_data/2728529-6534-expec0_pred1.csv'
 i=0
 with open(path,'w') as csv_file:
     csv_reader = csv.writer(csv_file, delimiter=',')
 
     line_count = 0
-    for i in range(2000):
-        csv_reader.writerow([test_bp[i, 1], (pred_t[i, 1].data.numpy())[0]])
+    for i in range(test_len):
+        csv_reader.writerow([test_bp[i, 1], (pred_t[i, 1].data.cpu().numpy())[0]])
 
 
 # plot the error of a single example
 samp_loss_vec = []
-for i in range(80):
-    a = pred_t[i,1].data.numpy()
+for i in range(test_len):
+    a = pred_t[i,1].data.cpu().numpy()
     b = test_bp[i,1]
     samp_loss_vec.append(abs(np.subtract(a[0],b)))
 plt.plot(samp_loss_vec)
