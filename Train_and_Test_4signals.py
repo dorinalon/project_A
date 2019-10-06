8080b547aee2aa371bff321657e7b95150cbb466
shirili <shirili.s@gmail.com>
2019-10-06 17:22:57 +0300

    train and test 4 signals 10 min preactice

--- Train_and_Test_4signals.py
+++ Train_and_Test_4signals.py
@@ -18,7 +18,7 @@ input_dim = 3
 hidden_size = 30
 num_layers = 3
 
-only_PPG_path = '/home/shirili/Downloads/ShirirliDorin/project_A/out_data_all_sig/'
+only_PPG_path = '/home/shirili/Downloads/ShirirliDorin/project_A/out_data_4_sig_6_10/'
 
 def read_csv(path):
     i = 0
@@ -35,7 +35,7 @@ def read_csv(path):
             i=i+1
     return row_count, data
 
-def calc_loss_precentage(expec_BP, pred_BP, time_length, iteration_num, patient_num):
+def calc_loss_precentage(expec_BP, pred_BP, time_length, iteration_num, patient_num,train_start_time):
     interval_30_sec = 3700
     expec_95 = []
     expec_5 = []
@@ -51,10 +51,13 @@ def calc_loss_precentage(expec_BP, pred_BP, time_length, iteration_num, patient_
     plt.plot(expec_5, label='expec_5')
     plt.plot(pred_95, label='pred_95')
     plt.plot(pred_5, label='pred_5')
+    plt.xlabel('mean value for every 0.5 minute ')
+    plt.ylabel('mmHg')
+    plt.title(" BP Prediction based on PPG, RI, ECG,systolic and diastolic, iteration " + str(iteration_num))
     plt.legend()
-    plt.savefig(only_PPG_path + patient_num + '/Prediction and Expected BP systolic and diastolic ' + str(iteration_num))
+    plt.savefig(str(only_PPG_path) + str(patient_num) + '_'+str(train_start_time) + '/Prediction and Expected BP systolic and diastolic ' + str(iteration_num))
 
-def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, test1_bp_path,test1_ppg_path,test1_ecg_path,test1_ri_path):
+def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, train_start_time):
 
     row_count_bp, data_bp = read_csv(path_bp)
     row_count_ppg, data_ppg = read_csv(path_ppg)
@@ -64,10 +67,10 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
 
     #test on other patients
 
-    row_count_bp_test1, data_bp_test_1 = read_csv(test1_bp_path)
-    row_count_ppg_test1, data_ppg_test_1 = read_csv(test1_ppg_path)
-    row_count_ecg_test1, data_ecg_test_1 = read_csv(test1_ecg_path)
-    row_count_ri_test1, data_ri_test_1 = read_csv(test1_ri_path)
+    # row_count_bp_test1, data_bp_test_1 = read_csv(test1_bp_path)
+    # row_count_ppg_test1, data_ppg_test_1 = read_csv(test1_ppg_path)
+    # row_count_ecg_test1, data_ecg_test_1 = read_csv(test1_ecg_path)
+    # row_count_ri_test1, data_ri_test_1 = read_csv(test1_ri_path)
 
     row_count = min(row_count_ppg,row_count_bp, row_count_ecg, row_count_ri)
 
@@ -96,21 +99,24 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
     train_size_125Hz = 16000 # round(row_count * (3 / 4))
     test_size_125Hz = 8000 # round(row_count / 4)
 
+    train_start_samp = train_start_time*60*125
+    train_size_125Hz = 10*60*125
+    test_size_125Hz = 5*60*125
 
-    train_bp = data_bp[8000:train_size_125Hz, :]
-    train_ppg = data_ppg[8000:train_size_125Hz, :]
-    train_ecg = data_ecg[8000:train_size_125Hz, :]
-    train_ri = data_ri[8000:train_size_125Hz, :]
+    train_bp = data_bp[train_start_samp:train_start_samp+train_size_125Hz, :]
+    train_ppg = data_ppg[train_start_samp:train_start_samp+train_size_125Hz, :]
+    train_ecg = data_ecg[train_start_samp:train_start_samp+train_size_125Hz, :]
+    train_ri = data_ri[train_start_samp:train_start_samp+train_size_125Hz, :]
 
-    test_bp = data_bp[train_size_125Hz:train_size_125Hz+test_size_125Hz, :]
-    test_ppg = data_ppg[train_size_125Hz:train_size_125Hz+test_size_125Hz, :]
-    test_ecg = data_ecg[train_size_125Hz:train_size_125Hz + test_size_125Hz, :]
-    test_ri = data_ri[train_size_125Hz:train_size_125Hz + test_size_125Hz, :]
+    test_bp = data_bp[train_start_samp+train_size_125Hz:train_start_samp+train_size_125Hz+test_size_125Hz, :]
+    test_ppg = data_ppg[train_start_samp+train_size_125Hz:train_start_samp+train_size_125Hz+test_size_125Hz, :]
+    test_ecg = data_ecg[train_start_samp+train_size_125Hz:train_start_samp+train_size_125Hz + test_size_125Hz, :]
+    test_ri = data_ri[train_start_samp+train_size_125Hz:train_start_samp+train_size_125Hz + test_size_125Hz, :]
 
-    data_bp_test_1 = data_bp_test_1[:test_size_125Hz,:]
-    data_ppg_test_1=data_ppg_test_1[:test_size_125Hz,:]
-    data_ecg_test_1 = data_ecg_test_1[:test_size_125Hz, :]
-    data_ri_test_1 = data_ri_test_1[:test_size_125Hz, :]
+    # data_bp_test_1 = data_bp_test_1[:test_size_125Hz,:]
+    # data_ppg_test_1=data_ppg_test_1[:test_size_125Hz,:]
+    # data_ecg_test_1 = data_ecg_test_1[:test_size_125Hz, :]
+    # data_ri_test_1 = data_ri_test_1[:test_size_125Hz, :]
 
     # SKlearn min max scaler (normalization)
 
@@ -126,18 +132,27 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
     scaler_PPG = MinMaxScaler(feature_range=(-1,1))
     train_ppg_scaled = scaler_PPG.fit_transform(train_ppg)
 
-    scaler_t_BP = MinMaxScaler(feature_range=(-1,1))
-    test_bp_scaled = scaler_t_BP.fit_transform(test_bp)
+    test_bp_scaled = scaler_BP.transform(test_bp)
 
-    scaler_t_ECG = MinMaxScaler(feature_range=(-1,1))
-    test_ecg_scaled = scaler_t_ECG.fit_transform(test_ecg)
+    test_ecg_scaled = scaler_ECG.fit_transform(test_ecg)
 
-    scaler_t_RI = MinMaxScaler(feature_range=(-1,1))
-    test_ri_scaled = scaler_t_RI.fit_transform(test_ri)
+    test_ri_scaled = scaler_RI.fit_transform(test_ri)
 
-    scaler_t_PPG = MinMaxScaler(feature_range=(-1,1))
-    test_ppg_scaled = scaler_t_PPG.fit_transform(test_ppg)
+    test_ppg_scaled = scaler_PPG.fit_transform(test_ppg)
 
+    # plt.plot(train_ppg_scaled[:1000,0], label='train_ppg')
+    # plt.plot(train_ecg_scaled[:1000,0], label='train_ecg')
+    # plt.plot(train_ri_scaled[:1000,0], label='train_ri')
+    # plt.plot(train_bp_scaled[:1000,0], label='train_BP')
+    # plt.legend()
+    # plt.show()
+    # plt.clf()
+    # plt.plot(test_ppg_scaled[:1000,0], label='test_ppg')
+    # plt.plot(test_ecg_scaled[:1000,0], label='test_ecg')
+    # plt.plot(test_ri_scaled[:1000,0], label='test_ri')
+    # plt.plot(test_bp_scaled[:1000,0], label='test_BP')
+    # plt.legend()
+    # plt.show()
     # Creating lstm class
     class CustomLSTM(nn.Module):
         # Ctor
@@ -190,7 +205,7 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
     t_inp_ri = torch.Tensor(test_ri_scaled.reshape((test_ri_scaled.shape[0], -1, 1))).cuda()
     x_t = torch.cat((t_inp_ecg, t_inp_ri, t_inp_ppg), dim=2)
 
-    for t in range(2):
+    for t in range(301):
         hidden = None
         x = torch.cat((inp_ppg, inp_ecg, inp_ri), dim=2)
         pred = r(x)
@@ -204,7 +219,7 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
         running_loss += loss.item()
         loss_vec.append(running_loss)
         print(t, running_loss)
-        if t%50==0:
+        if t%40==0:
 
             plt.clf()
             plt.plot(inp_ppg[:1000,0].data.cpu().numpy(),label='train_ppg')
@@ -214,7 +229,7 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
             plt.plot(out[:1000,0].data.cpu().numpy(), label='train_BP')
             plt.title('iteration '+str(t))
             plt.legend()
-            plt.savefig(only_PPG_path + patient_num + '/iteration_' + str(t))
+            plt.savefig(str(only_PPG_path) + str(patient_num) +'_'+str(train_start_time)+ '/iteration_' + str(t) )
 
             pred_t = r(x_t)
 
@@ -225,9 +240,9 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
             plt.ylabel('mmHg')
             plt.title(" BP Prediction based on PPG, RI, ECG, iteration " + str(t))
             plt.legend()
-            plt.savefig(only_PPG_path + patient_num + '/Prediction and Expected BP ' + str(t))
+            plt.savefig(str(only_PPG_path) + str(patient_num) +'_'+str(train_start_time)+ '/Prediction and Expected BP ' + str(t))
 
-            calc_loss_precentage(test_bp_scaled[:,1], pred_t[:,1].data.cpu().numpy(), test_size_125Hz, t, patient_num)
+            calc_loss_precentage(test_bp_scaled[:,1], pred_t[:,1].data.cpu().numpy(), test_size_125Hz, t, patient_num,train_start_time)
 
     # Mean Loss
     plt.clf()
@@ -235,7 +250,7 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
     plt.title("Mean loss")
     plt.xlabel('iteration')
     plt.ylabel('loss')
-    plt.savefig(only_PPG_path + patient_num + '/Mean_loss')
+    plt.savefig(str(only_PPG_path) + str(patient_num) +'_'+ str(train_start_time)+ '/Mean_loss')
 
     # TEST- on the same patient as the train
 
@@ -266,29 +281,37 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
     #     a = pred_t[j,1].data.cpu().numpy()
     #     pred_t[j,1] = (((a[0] + ((max_bp-min_bp)/2)+min_bp) * max_bp_div) * 0.0625) - 40
     # test_bp[:,1] = (((test_bp[:,1] + ((max_bp-min_bp)/2)+min_bp ) * max_bp_div) * 0.0625) - 40
-    # scaler_BP.inverse_transform(pred_t.data.cpu().numpy())
-    # scaler_BP.inverse_transform(test_bp.data.cpu().numpy())
-    # TODO: inverse the pred and test back to original
+    # print(str(type(pred_t)))
+    # print(str(type(pred_t.data.cpu())))
+    # print(str(np.shape(pred_t)))
+    # print(str(np.shape(pred_t.data.cpu())))
+    # print(str(pred_t[:,1].data.cpu()))
+    pred_t = scaler_BP.inverse_transform(pred_t[:,:,0].data.cpu())
+    test_bp_scaled = scaler_BP.inverse_transform(test_bp_scaled)
+    pred_t = (pred_t*0.0625)-40
+    test_bp_scaled = (test_bp_scaled * 0.0625) - 40
+    test_bp = (test_bp * 0.0625) - 40
 
     plt.clf()
-    plt.plot(pred_t[:1000,1].data.cpu().numpy(), label ='prediction_BP')
+    plt.plot(pred_t[:1000,1], label ='prediction_BP')
     plt.plot(test_bp_scaled[:1000,1], label ='expected_BP')
     plt.xlabel('samples')
     plt.ylabel('mmHg')
     plt.title(" BP Prediction based on PPG, RI, ECG")
     plt.legend()
-    plt.savefig(only_PPG_path + patient_num + '/Prediction and Expected BP')
+    plt.savefig(str(only_PPG_path) + str(patient_num) +'_' +str(train_start_time) +'/FINAL: Prediction and Expected BP')
+    calc_loss_precentage(test_bp_scaled[:, 0], pred_t[:, 0], test_size_125Hz,'orig value ', patient_num,train_start_time)
+    #calc_loss_precentage(test_bp_scaled[:, 1], pred_t[:, 1].data.cpu().numpy(), test_size_125Hz, t, patient_num)
 
-
-    path = only_PPG_path + patient_num + '/expec0_pred1.csv'
+    path = str(only_PPG_path) + str(patient_num) +'_'+ str(train_start_time)+'/expec0_pred1.csv'
     i=0
     with open(path,'w') as csv_file:
         csv_reader = csv.writer(csv_file, delimiter=',')
         line_count = 0
         for i in range(test_size_125Hz):
-            csv_reader.writerow([test_bp[i, 1], (pred_t[i, 1].data.cpu().numpy())[0]])
+            csv_reader.writerow([test_bp[i, 1], pred_t[i, 1]])
 
-
+    print("finised "+str(patient_num))
     # path = only_PPG_path + patient_num + '/expec0_pred1_test_on_patient1.csv'
     # i=0
     # with open(path,'w') as csv_file:
@@ -312,26 +335,56 @@ def Train_and_Test_4signals(path_bp, path_ppg, path_ecg, path_ri,patient_num, te
     # plt.savefig(only_PPG_path + patient_num + '/Error')
 
 
+
+
+Train_and_Test_4signals('/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2642420-8086/2642420-8086_BP_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2642420-8086/2642420-8086_PPG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2642420-8086/2642420-8086_ECG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2642420-8086/2642420-8086_RI_125Hz.csv',
+                        '2642420-8086',0)
+
+Train_and_Test_4signals('/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2642420-8086/2642420-8086_BP_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2642420-8086/2642420-8086_PPG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2642420-8086/2642420-8086_ECG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2642420-8086/2642420-8086_RI_125Hz.csv',
+                        '2642420-8086',15)
+
+Train_and_Test_4signals('/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2728529-6534/2728529-6534_BP_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2728529-6534/2728529-6534_PPG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2728529-6534/2728529-6534_ECG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2728529-6534/2728529-6534_RI_125Hz.csv',
+                        '2728529-6534',0)
+
+Train_and_Test_4signals('/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2728529-6534/2728529-6534_BP_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2728529-6534/2728529-6534_PPG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2728529-6534/2728529-6534_ECG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2728529-6534/2728529-6534_RI_125Hz.csv',
+                        '2728529-6534',10)
+
 Train_and_Test_4signals('/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_BP_125Hz.csv',
                         '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_PPG_125Hz.csv',
                         '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_ECG_125Hz.csv',
                         '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_RI_125Hz.csv',
-                       '2544444-5692',
-                       '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_BP_125Hz.csv',
-                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_PPG_125Hz.csv',
-                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_ECG_125Hz.csv',
-                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_RI_125Hz.csv'
+                       '2544444-5692',0
+                        )
+
+Train_and_Test_4signals('/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_BP_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_PPG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_ECG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_RI_125Hz.csv',
+                       '2544444-5692',15
                         )
 
 Train_and_Test_4signals('/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_BP_125Hz.csv',
                         '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_PPG_125Hz.csv',
                         '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_ECG_125Hz.csv',
                         '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_RI_125Hz.csv',
-                        '2210563-9215',
-                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_BP_125Hz.csv',
-                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_PPG_125Hz.csv',
-                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_ECG_125Hz.csv',
-                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2544444-5692/2544444-5692_RI_125Hz.csv')
+                        '2210563-9215',0)
 
+Train_and_Test_4signals('/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_BP_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_PPG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_ECG_125Hz.csv',
+                        '/home/shirili/Downloads/ShirirliDorin/project_A/data_4_sig/2210563-9215/2210563-9215_RI_125Hz.csv',
+                        '2210563-9215',15)
 
 
